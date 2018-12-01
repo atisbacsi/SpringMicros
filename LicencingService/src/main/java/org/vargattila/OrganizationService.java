bedd5260fc46c908333e731ad994a05b2e4f4744
atisbacsi <L4b4tl4N>
2018-12-01 17:27:15 +0100

    dec 1 Commit

--- LicencingService/src/main/java/org/vargattila/OrganizationService.java
+++ LicencingService/src/main/java/org/vargattila/OrganizationService.java
@@ -1,7 +1,6 @@
 package org.vargattila;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
