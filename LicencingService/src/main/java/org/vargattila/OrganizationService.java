d465d8a48fbee016ed62e2205aa186834485dc0d
Attila Varga <vargattila@gmail.com>
2018-12-02 11:44:39 +0100

    Commit at Home - HystrixCommand

--- LicencingService/src/main/java/org/vargattila/OrganizationService.java
+++ LicencingService/src/main/java/org/vargattila/OrganizationService.java
@@ -1,17 +1,43 @@
 package org.vargattila;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class OrganizationService {
     @Autowired
     RestTemplate restTemplate;
 
-    @HystrixCommand
-    public Organization getOrganizationById(String id){
+    @HystrixCommand(
+            commandProperties = {
+                    @HystrixProperty(
+                            name = "execution.isolation.thread.timeoutInMilliseconds",
+                            value = "1000"
+                    )
+            },
+            fallbackMethod = "getFallbackOrganization"
+    )
+    public Organization getOrganizationById(String id) {
+
+        try {
+            int anInt = ThreadLocalRandom.current().nextInt(0, 3);
+            if (anInt > 1) {
+                Thread.sleep(11000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return restTemplate.getForObject("http://organizationservice/orgById/{id}", Organization.class, id);
     }
+    private Organization getFallbackOrganization(String id){
+        Organization organization = new Organization();
+        organization.setContactName("This is a fallback organization");
+        return organization;
+    }
+
 }
